724477102408ba6db08ac2c55ba4cc7097dbfca5
ryanchun91 <ryan.salrin.chun@gmail.com>
2022-03-29 23:13:40 +0000

    sudo added on userdata

--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Install necessary tools
-yum install httpd wget unzip epel-release mysql -y
-yum -y install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum-config-manager --enable remi-php74
-yum install php -y
-yum install php-mysql -y
+sudo yum install httpd wget unzip epel-release mysql -y
+sudo yum -y install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum-config-manager --enable remi-php74
+sudo yum install php -y
+sudo yum install php-mysql -y
 # Download latest wordpress
 wget https://wordpress.org/latest.tar.gz
 tar -xf latest.tar.gz -C /var/www/html/
@@ -14,5 +14,5 @@ sed 's/SELINUX=permissive/SELINUX=enforcing/g' /etc/sysconfig/selinux -i
 setenforce 0
 chown -R apache:apache /var/www/html/
 # Start web server
-systemctl restart httpd
-systemctl enable httpd
+sudo systemctl restart httpd
+sudo systemctl enable httpd
